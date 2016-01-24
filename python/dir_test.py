7095d157a3e98f5f33da90ca8a1510c7c0197e22
ben <bkurtz@users.noreply.github.com>
2016-01-23 23:53:06 -0800

    protobuf file and python code to read directories
    Doing it the right way instead of the sketchy way that the original code does!
    Still unsure what the third date field is

---
+++ python/dir_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Usage:
+# dir_test.py dir_file
+# polar_downloader / 2>/dev/null | dir_test.py
+
+import directory_pb2
+import sys
+
+def date_to_string(d):
+	return "%04i-%02i-%02i %02i:%02i:%02i +%i" % (d.date.year, d.date.month, d.date.day, d.time.hour, d.time.minute, d.time.second, d.tz)
+
+if len(sys.argv) > 1:
+	fn = sys.argv[1]
+	with open(fn, 'r') as myfile:
+		data=myfile.read()
+else:
+	data=sys.stdin.read()
+
+directory = directory_pb2.Directory()
+directory.ParseFromString(data)
+
+for item in directory.item:
+	print("%s	%i" % (item.name, item.size))
+	print("  created:  %s" % (date_to_string(item.created)))
+	print("  modified: %s" % (date_to_string(item.modified)))
+	print("  long ago: %s" % (date_to_string(item.d3)))
