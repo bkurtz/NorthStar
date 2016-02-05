567e6030980a7987bc24c1a3e5fc35d5165533f3
ben <bkurtz@users.noreply.github.com>
2016-02-04 20:51:27 -0800

    new script to print directory contents recursively

---
+++ python/dir_recursive.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# Usage:
+# dir_recursive.py /
+
+
+import directory_pb2
+import sys
+import subprocess
+
+def date_to_string(d):
+	if(d.isUTC):
+		tz = 'Z'
+	else:
+		tz = "%+i" % (d.tz_minutes/60)
+	return "%04i-%02i-%02i %02i:%02i:%02i %s" % (d.date.year, d.date.month, d.date.day, d.time.hour, d.time.minute, d.time.second, tz)
+
+if len(sys.argv) > 1:
+	fn = sys.argv[1]
+else:
+	sys.exit(1)
+
+# determine the location of the polar_downloader binary
+polar_downloader = "/Users/ben/Downloads/polar/v800_downloader/src/usb/polar_downloader"
+
+def recursive_download_and_print(path):
+	data = subprocess.check_output([polar_downloader, path]);
+	
+	directory = directory_pb2.Directory()
+	directory.ParseFromString(data)
+	
+	for item in directory.item:
+		print("%s%s	%i" % (path, item.name, item.size))
+		print("  created:  %s" % (date_to_string(item.created)))
+		print("  modified: %s" % (date_to_string(item.modified)))
+		print("  long ago: %s" % (date_to_string(item.d3)))
+	
+	for item in directory.item:
+		if item.name.endswith('/'):
+			recursive_download_and_print(path + item.name)
+
+recursive_download_and_print(fn)
